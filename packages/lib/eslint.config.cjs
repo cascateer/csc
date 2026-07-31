4af93316a2e5226ed057a3fa942462eb99653b4d
cascateer <cascateer@gmail.com>
2026-07-31 18:34:18 +0200

    package jsons

--- packages/lib/eslint.config.cjs
+++
@@ -1,4 +0,0 @@
-const { config } = require("@cascateer/eslint-config");
-const { defineConfig } = require("eslint/config");
-
-module.exports = defineConfig(config);
--- packages/lib/src/array.ts
+++ packages/lib/src/array.ts
@@ -1,4 +1,4 @@
 export type MaybeArray<T> = T | T[];
 
-export const asArray = <T>(array: MaybeArray<T>): Array<T> =>
+export const asArray = <T>(array: MaybeArray<T>): T[] =>
   Array.isArray(array) ? array : [array];
