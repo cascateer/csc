214f97c70f18feb545287a496815e636ba61a907
cascateer <cascateer@gmail.com>
2026-08-05 14:25:16 +0200

    cube

--- packages/demo/src/slices/app/index.tsx
+++ packages/demo/src/slices/app/index.tsx
@@ -1,7 +1,13 @@
 import { AppCounterComponent } from "./counter/component";
+import { AppCubeComponent } from "./cube/component";
 import { appSlice } from "./slice";
 
 export const AppRootComponent = appSlice
   .createComponent("root")
   .withStyles(import("./styles.scss?inline"))
-  .withTemplate(() => () => <AppCounterComponent />);
+  .withTemplate(() => () => (
+    <>
+      <AppCubeComponent />
+      <AppCounterComponent />
+    </>
+  ));
--- packages/demo/src/slices/app/slice.ts
+++ packages/demo/src/slices/app/slice.ts
@@ -1,5 +1,8 @@
 import { createSlice } from "@cascateer/core";
-import { map } from "rxjs";
+import { modulo } from "@cascateer/lib/math";
+import { combineLatest, map } from "rxjs";
+import { add } from "./cube/math";
+import { Cube } from "./cube/types";
 
 export const appSlice = await createSlice("app")
   .withStore(import("./store"))
@@ -7,10 +10,44 @@ export const appSlice = await createSlice("app")
   .withTerminal(({ TerminalProvider }) =>
     new TerminalProvider()
       .provideEffects(({ effect }) => ({
-        counterValue: effect<number, number>(
+        counter__value: effect<number, number>(
           ({ store }) =>
             (offset) =>
-              store.effects.counterValue().pipe(map((value) => value + offset)),
+              store.effects.counter__value().pipe(map(add(offset))),
+        ),
+        cube__currentBaseActionParity: effect<void, Cube.BaseActionParity>(
+          ({ store }) =>
+            () =>
+              store.effects
+                .cube__currentBaseActionIndex()
+                .pipe(
+                  map((index) => (modulo(index, 2) === 1 ? "odd" : "even")),
+                ),
+        ),
+        cube__currentBaseAction: effect<void, Cube.BaseAction | undefined>(
+          ({ store }) =>
+            () =>
+              combineLatest([
+                store.effects.cube__baseActionQueue(),
+                store.effects.cube__currentBaseActionIndex(),
+              ]).pipe(map(([queue, index]) => queue[index])),
+        ),
+        cube__currentSliceActions: effect<void, Cube.SliceAction[]>(
+          ({ store }) =>
+            () =>
+              combineLatest([
+                store.effects.cube__baseActionQueue(),
+                store.effects.cube__currentBaseActionIndex(),
+              ]).pipe(map(([queue, index]) => queue.slice(0, index).flat())),
+        ),
+      }))
+      .provideEffects(({ effect }) => ({
+        cube__layout: effect<void, Cube.Layout>(
+          ({ terminal }) =>
+            () =>
+              terminal.effects
+                .cube__currentSliceActions()
+                .pipe(map((actions) => new Cube.Layout().apply(...actions))),
         ),
       }))
       .complete(),
